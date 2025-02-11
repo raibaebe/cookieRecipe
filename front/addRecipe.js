1fd3f0001b0797c2f6bfb31bd945beacedebf2cc
ALHNk <88340494+ALHNk@users.noreply.github.com>
2025-02-11 23:14:06 +0500

    I am stupido forgot about front-end

--- front/addRecipe.js
+++ front/addRecipe.js
@@ -5,15 +5,32 @@ document.getElementById('add-recipe-form').addEventListener('submit', async func
     const ingredients = document.getElementById('ingredients').value;
     const instructions = document.getElementById('instructions').value;
     const status = document.getElementById('status').value;
+    const imageInput = document.getElementById('image');
     const authorid = localStorage.getItem('user_id');
     alert('author:', authorid);
 
+    const formData = new FormData();
+    formData.append('image', imageInput.files[0]); 
+
+    // Upload the image first
+    const imageUploadResponse = await fetch('http://localhost:5000/recepie/upload-image', {
+        method: 'POST',
+        body: formData
+    });
+
+    if (!imageUploadResponse.ok) {
+        alert('Failed to upload image.');
+        return;
+    }
+
+    const imageData = await imageUploadResponse.json(); 
+    
     const response = await fetch('http://localhost:5000/recepie/add', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ title, ingredients, instructions, authorid})
+        body: JSON.stringify({ title, ingredients, instructions, authorid, imagePath: imageData.path })
     });
 
     if (response.ok) {
